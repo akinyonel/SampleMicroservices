d75a851d87f7208483a7fe51079dda41425ff9f0
akinyonel1 <a.yonel@extratik.com>
2021-07-06 15:48:59 +0300

    Added CourseService and ICourseServise

--- Services/Catalog/FreeCourseServices.Catalog/Services/CourseService.cs
+++ Services/Catalog/FreeCourseServices.Catalog/Services/CourseService.cs
@@ -11,7 +11,7 @@ using System.Threading.Tasks;
 
 namespace FreeCourseServices.Catalog.Services
 {
-    internal class CourseService
+    internal class CourseService : ICourseService
     {
         private readonly IMongoCollection<Course> _courseCollection;
         private readonly IMongoCollection<Category> _categoryCollection;
@@ -83,9 +83,45 @@ namespace FreeCourseServices.Catalog.Services
             return Response<List<CourseDto>>.Success(_mapper.Map<List<CourseDto>>(courses), 200);
         }
 
-        //public async Task<Response<CourseDto>> CreateAsync(CourseCreateDto courseCreateDto)
-        //{
+        public async Task<Response<CourseDto>> CreateAsync(CourseCreateDto courseCreateDto)
+        {
+            var newCourse = _mapper.Map<Course>(courseCreateDto);
 
-        //}
+            newCourse.CreatedTime = DateTime.Now;
+            await _courseCollection.InsertOneAsync(newCourse);
+
+            return Response<CourseDto>.Success(_mapper.Map<CourseDto>(newCourse), 200);
+        }
+
+        public async Task<Response<NoContent>> UpdateAsync(CourseUpdateDto courseUpdateDto)
+        {
+            var updateCourse = _mapper.Map<Course>(courseUpdateDto);
+
+            var result = await _courseCollection.FindOneAndReplaceAsync(x => x.Id == courseUpdateDto.Id, updateCourse);
+
+            if (result== null)
+            {
+                return Response<NoContent>.Fail("Course Not Found", 404);
+            }
+
+            return Response<NoContent>.Success(204);
+
+          
+        }
+
+        public async Task<Response<NoContent>> DeleteAsync(string id)
+        {
+            var result = await _courseCollection.DeleteOneAsync(x => x.Id == id);
+
+            if (result.DeletedCount > 0)
+            {
+                return Response<NoContent>.Success(204);
+            }
+            else
+            {
+                return Response<NoContent>.Fail("Course Not Found", 404);
+            }
+
+        }
     }
 }
