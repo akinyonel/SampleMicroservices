3b9cd17e6bd3a47b309d58f0f0733738edc5cd75
akinyonel1 <a.yonel@extratik.com>
2021-07-06 15:10:02 +0300

    Add project files.

---
+++ Services/Catalog/FreeCourseServices.Catalog/Model/Feature.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FreeCourseServices.Catalog.Model
+{
+    public class Feature
+    {
+        public int Duration { get; set; }
+
+    }
+}
